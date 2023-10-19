57c2388dddbc43568a7f6cee385bf67d73c2580c
Vladimir Kravets <natccorp@gmail.com>
2023-10-19 17:02:57 +0300

    Initial Commit

---
+++ TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by Vladimir Kravets on 19.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
